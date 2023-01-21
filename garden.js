f873b8082afcc8534b51a8f7bc37e7e42ad40d37
Mu <muyangyan1@gmail.com>
2023-01-21 05:06:11 -0500

    clickable grid

---
+++ garden.js
@@ -0,0 +1,16 @@
+class Garden {
+
+    grid = [];
+    cellSize;
+    width;
+    height;
+
+    constructor(width, height, cellSize) {
+        for (let i = 0; i < height; i++) {
+            this.grid.push(new Array(width));
+        }
+        this.cellSize = cellSize;
+        this.width = width;
+        this.height = height;
+    }
+}
--- index.js
+++ index.js
@@ -39,47 +39,29 @@ const stage = new Konva.Stage({
 const layer = new Konva.Layer();
 stage.add(layer);
 
-const gridSize = 30;
+const GARDEN_X = 50;
+const GARDEN_Y = 50;
+
+let garden = new Garden(20,20,50);
 // double for to create every grid in the window
-for(w = 0; w < window.innerWidth; w += gridSize) {
-  for (h = 0; h < window.innerHeight; h += gridSize) {
-    const grid = new Konva.Rect({
-      x: w,
-      y: h,
+for(let w = 0; w < garden.width; w++) {
+  for (let h = 0; h < garden.height; h++) {
+    const cell = new Konva.Rect({
+      x: GARDEN_X + w * garden.cellSize,
+      y: GARDEN_Y + h * garden.cellSize,
       fill: "#79e7a4",
-      height: gridSize,
-      width: gridSize,
-    });
-    layer.add(grid)
-
-    // adds horizontal line
-    const horizontalLine = new Konva.Line({
-      points: [0, h, window.innerWidth, h],
+      height: garden.cellSize,
+      width: garden.cellSize,
       stroke: 'black',
-      strokeWidth: 2,
-      lineCap: 'round',
-      lineJoin: 'round',
-    })
-    layer.add(horizontalLine)
+      strokeWidth: 2
+    });
+    layer.add(cell)
     
-
     // mouse listeners
     // this.x() / gridSize lets Vincent's DB take it as (1, 0) (17, 11)
-    grid.on('mousedown', function () {
-      console.log("mouse down", this.x() / gridSize, this.y() / gridSize);
-    });
-    grid.on('mouseup', function () {
-      console.log("mouse up", this.x() / gridSize, this.y() / gridSize);
+    cell.on('click', function () {
+      console.log("mouse down", (this.x() - GARDEN_X) / garden.cellSize, (this.y() - GARDEN_Y) / garden.cellSize);
     });
   }
-  // adds vertical line for separation
-  const verticalLine = new Konva.Line({
-    points: [w, 0, w, window.innerHeight],
-    stroke: 'black',
-    strokeWidth: 2,
-    lineCap: 'round',
-    lineJoin: 'round',
-  })
-  layer.add(verticalLine)
 
 }
