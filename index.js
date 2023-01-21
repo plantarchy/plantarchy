3092971927b7c8f2c989ceabb14f4256cdc342ce
Connor Barnsley <ctbarnsley@icloud.com>
2023-01-21 00:55:08 -0500

    grid and colors

--- index.js
+++ index.js
@@ -10,13 +10,34 @@ const layer = new Konva.Layer();
 stage.add(layer);
 
 
-const rect = new Konva.Rect({
-  x: 50,
-  y: 50,
-  fill: "blue",
-  height: 100,
-  width: 200,
-});
 
-layer.add(rect)
+for(w = 0; w < window.innerWidth; w += 30) {
+  for (h = 0; h < window.innerHeight; h += 30) {
+    const grid = new Konva.Rect({
+      x: w,
+      y: h,
+      fill: "#79e7a4",
+      height: 100,
+      width: 2000,
+    });
+    layer.add(grid)
+    const horizontalLine = new Konva.Line({
+      points: [0, h, window.innerWidth, h],
+      stroke: 'black',
+      strokeWidth: 2,
+      lineCap: 'round',
+      lineJoin: 'round',
+    })
+    layer.add(horizontalLine)
+    
+  }
+  const verticalLine = new Konva.Line({
+    points: [w, 0, w, window.innerHeight],
+    stroke: 'black',
+    strokeWidth: 2,
+    lineCap: 'round',
+    lineJoin: 'round',
+  })
+  layer.add(verticalLine)
+}
 
