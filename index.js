c1398e377fb4693ee39a0649493c217576d93014
Mu <muyangyan1@gmail.com>
2023-01-21 05:38:39 -0500

    boxes change color

--- index.js
+++ index.js
@@ -1,34 +1,36 @@
 const log = console.log;
 const API_URL = "https://c829-128-210-107-129.ngrok.io"
 async function Index() {
-    document.getElementById("konva-holder").style.display = "block";
-    document.getElementById("signup").style.display = "none";
-    const game_code = document.getElementById("game-code").value
-    const username = document.getElementById("name-input").value;
-    console.log(document.getElementById("name-input").value);
-    // const res = await fetch(API_URL + "/games");
-    // const data = await res.json();
-    // console.log(data)
-    const res = await fetch(API_URL + "/login", {
-      method: "POST",
-      headers: {
-        'Accept': 'application/json',
-        'Content-type': 'application/json'
-      },
-      body: JSON.stringify({
-        game_code: game_code,
-        player_name: username
-      })
-    });
-    if (res.status === 400) {
-      alert("Error 400");
-    }
-    if (res.status === 404) {
-      alert("Error 404");
-    }
-    const data = await res.json();
-    console.log(data);
-    window.playerID = data.player;
+  document.getElementById("konva-holder").style.display = "block";
+  document.getElementById("signup").style.display = "none";
+  const game_code = document.getElementById("game-code").value
+  const username = document.getElementById("name-input").value;
+  console.log(document.getElementById("name-input").value);
+  // const res = await fetch(API_URL + "/games");
+  // const data = await res.json();
+  // console.log(data)
+  const res = await fetch(API_URL + "/login", {
+    method: "POST",
+    headers: {
+      'Accept': 'application/json',
+      'Content-type': 'application/json'
+    },
+    body: JSON.stringify({
+      game_code: game_code,
+      player_name: username
+    })
+  });
+  if (res.status === 400) {
+    alert("Error 400");
+  }
+  if (res.status === 404) {
+    alert("Error 404");
+  }
+  const data = await res.json();
+  console.log(data);
+  window.playerID = data.player;
+
+  init();
 }
 const stage = new Konva.Stage({
   height: window.innerHeight,
@@ -44,24 +46,35 @@ const GARDEN_Y = 50;
 
 let garden = new Garden(20,20,50);
 // double for to create every grid in the window
-for(let w = 0; w < garden.width; w++) {
-  for (let h = 0; h < garden.height; h++) {
-    const cell = new Konva.Rect({
-      x: GARDEN_X + w * garden.cellSize,
-      y: GARDEN_Y + h * garden.cellSize,
-      fill: "#79e7a4",
-      height: garden.cellSize,
-      width: garden.cellSize,
-      stroke: 'black',
-      strokeWidth: 2
-    });
-    layer.add(cell)
-    
-    // mouse listeners
-    // this.x() / gridSize lets Vincent's DB take it as (1, 0) (17, 11)
-    cell.on('click', function () {
-      console.log("mouse down", (this.x() - GARDEN_X) / garden.cellSize, (this.y() - GARDEN_Y) / garden.cellSize);
-    });
-  }
 
+
+
+function init() {
+  for(let w = 0; w < garden.width; w++) {
+    for (let h = 0; h < garden.height; h++) {
+      const cell = new Konva.Rect({
+        x: GARDEN_X + w * garden.cellSize,
+        y: GARDEN_Y + h * garden.cellSize,
+        fill: "#79e7a4",
+        height: garden.cellSize,
+        width: garden.cellSize,
+        stroke: 'black',
+        strokeWidth: 2
+      });
+
+      // mouse listeners
+      // this.x() / gridSize lets Vincent's DB take it as (1, 0) (17, 11)
+      cell.on('click', function () {
+        cellX = (this.x() - GARDEN_X) / garden.cellSize;
+        cellY = (this.y() - GARDEN_Y) / garden.cellSize
+        console.log("mouse down", cellX, cellY);
+
+        garden.grid[cellX][cellY] = new Plant();
+        cell.setAttr("fill", "#ffffff");
+        cell.draw();
+      });
+      layer.add(cell)
+      }
+  }
+  layer.draw();
 }
